28e04669771ae5f52e01bd96362c9c7c8a5896f9
Jaiden Khosla <74748528+JaidenKhosla@users.noreply.github.com>
2025-04-21 16:23:59 -0500

    More work on banner

--- KeyClubWebsite/src/components/Home.tsx
+++ KeyClubWebsite/src/components/Home.tsx
@@ -1,10 +1,9 @@
 import "../stylesheets/Home.css"
-import HomeBanner from "../assets/homeBanner.jpg"
 
 export default function Home() {
     return <div className="Page">
         <div className="Banner">
-            <img src={HomeBanner}/>
+            <h2>What's up?</h2>
         </div>
     </div>
 }
