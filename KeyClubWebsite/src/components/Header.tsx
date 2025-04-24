6cbe0f42d8bcafeeddae8bb89aabc3fb902ea1c1
Jaiden Khosla <74748528+JaidenKhosla@users.noreply.github.com>
2025-04-24 14:14:19 +0000

    Home Page Banner

--- KeyClubWebsite/src/components/Header.tsx
+++ KeyClubWebsite/src/components/Header.tsx
@@ -2,11 +2,11 @@ import { useState } from "react"
 
 import "../stylesheets/Header.css"
 
-const routes : string[] = ["Home", "About", "Officers", "Gallary", "Contact"]
+const routes : string[] = ["Home", "About", "Officers", "Gallery", "Contact"]
 
 function getRoutes() {
     return routes.map((el, idx) => {
-        return <h2 className="headerOption" key={idx}>{el}</h2>
+        return <h2 className="headerOption white" key={idx}>{el}</h2>
     })
 }
 
@@ -14,11 +14,11 @@ export default function Header() {
 
     const [ burgerMenuToggle, setBurgerToggle ] = useState(false);
 
-    return (<div className="headerContainer"><div className="header">
+    return (<div className="headerContainer"><div className="header bannerBlue-bg">
             {getRoutes()}
-            {<h2 className="burgerMenuOption" onClick={ () => {setBurgerToggle(prev => !prev)}}>☰</h2>}
+            {<h2 className="burgerMenuOption white" onClick={ () => {setBurgerToggle(prev => !prev)}}>☰</h2>}
         </div>
-        <div className="burgerMenu" style={{opacity : burgerMenuToggle ? 1 : 0}}>
+        <div className="burgerMenu" style={{opacity : burgerMenuToggle ? 1 : 0, visibility : burgerMenuToggle ? "visible" : "hidden"}}>
             <br></br>
             <br></br>
             <br></br>
--- KeyClubWebsite/src/components/Home.tsx
+++ KeyClubWebsite/src/components/Home.tsx
@@ -1,9 +1,13 @@
 import "../stylesheets/Home.css"
 
+import keyClubLogo from "../assets/KeyClubLogo.png";
+
 export default function Home() {
     return <div className="Page">
         <div className="Banner">
-            <h2>What's up?</h2>
+            <img src={keyClubLogo}/>
+            <h1 className="white">James E. Taylor Key Club</h1>
+            <h3 className="white">Helping our community since 2011</h3>
         </div>
     </div>
 }
