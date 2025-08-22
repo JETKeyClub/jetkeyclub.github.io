686471f4471bad671bd137936485053039da03f5
Jaiden Khosla <74748528+JaidenKhosla@users.noreply.github.com>
2025-08-22 22:11:11 +0000

    SuspenseImage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Opportunity from "@/components/Opportunity/Opportunity";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      
+      <Opportunity image="/Assets/homeBanner.webp" description="" link=""/>
     </div>
   );
 }
--- components/Opportunity/Opportunity.tsx
+++ components/Opportunity/Opportunity.tsx
@@ -1,13 +1,64 @@
-"use client"
-
-import Image from "next/image";
+import SuspenseImage from "@/components/UI/SuspenseImage";
+import NextImage from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const WANTED_HEIGHT = 30;
 
 interface OpportunityProps {
     image: string,
-    details: string,
+    description: string,
     link?: string
 }
 
+const cache: Map<string, number[]> = new Map<string, number[]>();
+
+async function getBaseDimensions(src: string): Promise<number[]>{
+    
+    if(cache.has(src)) return cache.get(src)!;
+    
+    const image = new Image();
+    image.src = src;
+    
+    return new Promise((resolve, reject)=>{
+        image.onload = () => {
+            const dims = [image.width, image.height];
+            cache.set(src, dims);
+            resolve(dims);
+        }
+    })
+}
+
+"use client"
+function minimize(dimensions: number[]): [number[], number[]]{
+    const REM = Number.parseFloat(window.getComputedStyle(document.querySelector("html")!).fontSize);
+
+    const inRemScale = dimensions.map(e => e/REM);
+    const scaleFactor = WANTED_HEIGHT/inRemScale[1];
+    
+    const scaledRem = inRemScale.map(e => e*scaleFactor);
+    const scaledPixel =  scaledRem.map(e=>e*REM);
+
+    return [scaledRem, scaledPixel];
+}
+"use client"
+export default function Opportunity({ image, description, link }: OpportunityProps){
+    useEffect(()=>{
+        (async () => {
+            setDetails(minimize(await getBaseDimensions(image)));
+        })();
+    }, [image]);
+
+    const [ useDetails, setDetails ] = useState<[number[], number[]] | undefined>();
+
+    return <SuspenseImage 
+            src={image} 
+            width={(useDetails ? useDetails[1][0] : 600).toString()}
+            height={(useDetails ? useDetails[1][1] : 450).toString()}
+            alt={description}
+            className={
+                `w-[${useDetails ? useDetails[0][0] : 60}rem]
+                h-[${useDetails ? useDetails[0][1] : 45}]
+                `}
+            />
+}
