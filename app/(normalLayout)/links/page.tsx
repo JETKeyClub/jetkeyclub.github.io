d7796b6f01a2ec7a878634917048dc0c297335b1
Jaiden Khosla <74748528+JaidenKhosla@users.noreply.github.com>
2025-10-16 19:20:21 -0500

    Fixing Layout Change in Links

--- app/(normalLayout)/links/page.tsx
+++ app/(normalLayout)/links/page.tsx
@@ -4,7 +4,7 @@ export default function Page(){
     return (
         <div className="p-3">
             <h1 className="font-bold text-6xl">Links</h1>
-            <div className="mt-3">
+            <div className="mt-3 flex gap-x-3">
                 <Opportunity image="/Assets/Links/hourSubmission.png" description="Hour Submission Link" link="https://forms.gle/qgrULaVPF2eL7s7m6"/>
                 <Opportunity image="/Assets/Links/spreadsheet.png" description="Hour Submission Spreadsheet" link="https://docs.google.com/spreadsheets/d/1H2NqnDn8IL8N4Pm7MLy7MOLuv-r9zJpXTlWa-MGM_i8/edit?usp=sharing"/>
             </div>
